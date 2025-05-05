8830b4088724010e531f37d380d50fbfbcc2b06f
enkwi <iv.grechnev@gmail.com>
2025-05-05 16:45:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,260 +1,240 @@
-console.log("Lesson 1")
+// Q1
+// const array = [1, 2, 5, 7, 3, 200];
+// console.log([...array].map(array.pop, array));
 
-// # Модуль 1. Заняття 1. Змінні, типи та оператори
+//A1
+// const params = [1, 2, 5, 7, 3, 200]; // не міняти вхідні дані
 
-// ## Example 1 - Математичні оператори
+// const array = [...params];
 
-// Виведи на екран загальну кількість всіх яблук. 
-// кількість хороших яблук.
+// const reversedArr = params.map([].pop, array);
+// const reversedArr = params.reverse();
 
-// ```js
-const apples = 470;
-const badApples = 135;
-const halfApples = 67;
+// params.map([].pop, array); === params.map(array.pop, array)
 
-const harmfulApples = badApples + halfApples;
-console.log(harmfulApples);
+// console.log('IN:', params);
+// console.log('RES:', reversedArr);
 
-const goodApples = apples - harmfulApples;
-console.log(goodApples);
-// ```
+//Q2
+function makeSizer(size) {
+  return function () {
+    document.body.style.fontSize = size + 'px';
+  };
+}
 
-// ## Example 2 - Комбіновані оператори
+makeSizer(24);
 
-// Заміни вираз перевизначення комбінованим оператором `+=`.
+// var size12 = makeSizer(12);
+// var size14 = makeSizer(14);
+// var size16 = makeSizer(16);
 
-// ```js
-let students = 100;
-// students = students + 50;
-students += 50;
+// document.getElementById('size-12').onclick = size12;
+// document.getElementById('size-14').onclick = size14;
+// document.getElementById('size-16').onclick = size16;
 
-console.log(students);
-// ```
+//Q3
+// class Animal {
+//   #age = 10;
 
-// ## Example 3 - Пріоритет операторів
+//   static hasName(obj) {
+//     if (obj.name) return true;
+//     return false;
+//   }
 
-// Розбери пріоритет операторів в інструкції привласнення значення змінної
-// `result`.
+//   constructor({ type, name }) {
+//     (this.type = type), (this.name = name);
+//   }
 
-// ```js
-const result = 108 + 223 - 2 * 5;
-console.log(result);
-// ```
-
-// ## Example 4 - Клас Math
-
-// Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д. значення
-// змінної `value`. Використовуй методи `Math.floor()`, `Math.ceil()` та
-// `Math.round()`. Перевір що буде в консолі при значеннях `27.3` та `27.9`.
-
-// ```js
-const value = 27.7;
-const rounded = Math.round(value);
-const roundedDown = Math.floor(value);
-const roundedUp = Math.ceil(value);
-// ```
-console.log("Math.round", rounded);
-console.log("Math.roundedDown", roundedDown);
-console.log("Math.roundedUp", roundedUp);
-
-// ## Example 5 - Шаблонні рядки
-
-// Склади фразу за допомогою шаблонних рядків `A has B bots in stock`, де A, B -
-// змінні вставлені в рядок.
-
-// ```js
-const companyName = 'Cyberdyne Systems';
-const repairBots = 150;
-const defenceBots = 50;
-const totalBots = repairBots + defenceBots;
-const message = `${companyName} has ${totalBots} bots in stock`;
-console.log(message); // "Cyberdyne Systems has 200 bots in stock"
-// ```
-
-// ## Example 6 - Методи рядків та чейнінг
-
-// Напиши скрипт, який розраховує індекс маси тіла людини. Для цього
-// необхідно розділити вагу в кілограмах на квадрат висоти людини у метрах.
-
-// Вага та висота зберігаються у змінних `weight` та `height`, але не як числа, а в
-// вигляді рядків (спеціально для завдання). Не цілі числа можуть бути задані у вигляді
-// `24.7` або `24,7`, тобто як роздільник дробової частини може бути
-// кома.
-
-// Індекс маси тіла необхідно округлити до однієї цифри після коми;
-
-/**
- * 1. Знайти квадрат зросту людини //Math.pow()
- * 2. Розділити вагу на квадрат зросту. // звичайне ділення
- * 3. Округлити фінальний результат
- * 
- * Зробили:
- * 1. Запитали користувача ввести значення ваги і зросту
- * 2. Замінили можливу кому на крапку
- * 3. Явно конвертували рядок в число
- * 4. Знайшли квадрат зросту
- * 5. Знайшли індекс маси тіла
- * 6. Форматували індекс маси тіла до одніє цифри після крапки
- * 7. Привели значення індексу маси тіла до числового типу
- */
-// ```js
-
-// let weight = '88,3';
-// let height = '1.75';
-
-// const weightInt = Number(prompt('Введіть вагу в кілограмах').replace(',','.'));
-// const heightInt = Number(prompt('Введіть зріст в метрах').replace(',','.'));
-
-// console.log("heightInt = ",heightInt)
-// console.log("weightInt = ",weightInt)
-
-// const heightPow = Math.pow(heightInt, 2);
-// console.log(heightPow)
-
-// const bmi = weightInt / heightPow;
-// const roundedBmi = Number(bmi.toFixed(1));
-
-// console.log(roundedBmi); // 28.8
-// ```
-
-// ## Example 7 - Оператори порівняння та приведення типів
-
-// Яким буде результат виразів?
-console.log('\n==============');
-console.log(5 > 4); // true
-console.log(10 >= '7'); // true. 
-console.log('2' > '12'); // true. String порівнюються посимвольно по коду символа. Як отримати код символа https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
-console.log('2' < '12'); // false
-console.log('4' == 4); // true
-console.log('6' === 6); // false. 
-console.log('false' === false); // false
-console.log(1 == true);  // true. 
-console.log(1 === true); // false.
-console.log('0' == false); //true.
-console.log('0' === false); // false.
-console.log('Papaya' < 'papaya'); // true.
-const a = 'Papaya'.charCodeAt(0);
-const b = 'papaya'.charCodeAt(0);
-console.log(a,b)
-
-console.log('Papaya' === 'papaya'); // false. 
-console.log(undefined == null); // true.
-console.log(undefined === null); // false.
-// ```
-
-// ## Example 8 - Логічні оператори
-
-// Яким буде результат виразів?
-// && - Оператор приводить всі операнди до типу Boolean, і повертає перший який дасть false. Або останній, якщо false нема
-// || - Оператор приводить всі операнди до типу Boolean, і повертає перший який дасть true. Або останній, якщо true нема
-console.log(true && 3); // 3
-console.log(false && 3); // false
-console.log(1 && null && 2); // null
-console.log(true && 0 && 'kiwi'); // 0
-// true //true
-// 0 // false
-// 'kiwi' // true
-
-console.log(true || 3); // true
-console.log(true || 3 || 4); // true
-console.log(true || false || 7); // true
-console.log(null || 2 || undefined); // 2
-
-console.log((1 && null && 2) > 0); // false
-// 1 // true
-// null // false
-// 2 // true
-// null > 0
-/**
- * 1. Виконується оператор в дужках (1 && null && 2) = null
- * 2. Виконуються порівняння null > 0
- */
-
-console.log(2 && 3); // 3
-console.log(null || (2 && 3) || 4); // 3
-// 2 //true
-// 3 //true
-// 2 && 3 = 3
-
-// null //false
-// 3 // true
-// 4 // true
-/**
- * 1. Виконується оператор в дужках (2 && 3) = 3
- * 2. Виконується опеартор null || 3 || 4 = 3
- */
-
-
-
-// ```
-
-// ## Example 9 - Значення за замовчуванням та оператор нульового злиття
-
-// Отрефактори код так, щоб у змінну `value` присвоювалося значення
-// змінної `incomingValue`, якщо воно не рівне `undefined` або `null`. В
-// іншому випадку має присвоюватися значення `defaultValue`. Перевір роботу
-// скрипта для наступних значень змінної `incomingValue`: null, undefined, 0,
-// false. Використовуй оператор `??` (nullish coalescing operator).
-
-// ```js
-const incomingValue = null;
-const defaultValue = 10;
-const value1 = incomingValue ?? defaultValue;
-
-console.log(value1);
-// ```
-
-// ## Example 10 - Оператор % та методи рядків
-
-// Напиши скрипт, який переведе значення `totalMinutes` (кількість хвилин) в
-// рядок у форматі годин та хвилин `HH:MM`.
-
-// - 70 покаже 01:10
-// - 450 покаже 07:30
-// - 1441 покаже 24:01
-
-// ```js
-const totalMinutes = 450;
-
-const hours = Math.floor(totalMinutes/60);
-const minutes = totalMinutes % 60;
-
-const formatedTime = `${String(hours).padStart(2, '0')}:${minutes}`;
-console.log(formatedTime);
-
-const string = 'abc';
-console.log(string.padStart(4, '5'))
-
-// ```
-
-
-
-
-
-// let clientCounter = 18;
-// const maxClients = 25;
-
-// while (clientCounter < maxClients) {
-//   console.log(clientCounter);
-//   clientCounter += 1;
+//   sayHi() {
+//     console.log(
+//       `${this.name} say Hi to you! I am ${this.#age} years old`
+//     );
+//   }
 // }
 
+// const rabbit = new Animal({ type: 'rabbit', name: 'Pushok' });
+// const dog = new Animal({ type: 'dog', name: 'Sharik' });
+// const dogZevs = new Animal({ type: 'dog' });
 
-// let clientCounter = 18;
-// const maxClients = 25;
+// rabbit.sayHi();
+// dog.sayHi();
 
-// while (clientCounter < maxClients) {
-//     clientCounter += 1;
-//     console.log(clientCounter);
+// console.log(Animal.hasName(dogZevs));
+
+// Q4
+const user = {
+  name: 'John',
+  sayHi() {
+    console.log(`Hi, Mr ${this.name}`);
+  },
+  sayBye() {
+    console.log(`Bye, Mr`);
+  },
+};
+
+// user.sayHi(); //Hi, Mr John
+// user.sayBye(); //Bye, Mr
+
+// перший варіант виклику - виклик поза тернарним оператором.
+// (user.name === 'John' ? user.sayHi : user.sayBye)();
+
+// const method = user.name === 'John' ? user.sayHi : user.sayBye;
+// method();
+
+// user.name === 'John' ? user.sayHi() : user.sayBye(); // Hi, Mr John
+// user.name === 'Jack' ? user.sayHi() : user.sayBye(); // Bye, Mr
+
+// Q5
+
+const countWords = (words) => {
+  // 1
+  // const result = {};
+  // words.forEach((element) => {
+  //   if (result[element]) {
+  //     result[element] += 1;
+  //   } else {
+  //     result[element] = 1;
+  //   }
+  // });
+  // return result;
+  //2
+  // return words.reduce((obj, item) => {
+  //   obj.hasOwnProperty(item) ? (obj[item] += 1) : (obj[item] = 1);
+  //   return obj;
+  // }, {});
+
+  //3
+  return words.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+// const result = countWords([
+//   'Apple',
+//   'Banana',
+//   'Banana',
+//   'Orange',
+//   'Orange',
+//   'Orange',
+//   'Orange',
+// ]);
+// console.log(result);
+
+function wrapper() {
+  let counter = 0;
+
+  function inner() {
+    counter += 1;
+    return counter;
+  }
+
+  return inner;
+}
+
+// const func = wrapper();
+// func();
+// func();
+// func();
+// func();
+// const count = func();
+// console.log(count);
+
+// const func2 = wrapper();
+// const coun2 = func2();
+// console.log(coun2);
+
+// function addOne(number) {
+//   return function (n) {
+//     return number + n;
+//   };
 // }
 
+// const add = addOne(10);
+// const result = add(5);
+// console.log(result);
 
-// while (clientCounter < maxClients) {
-//   console.log(clientCounter);
-//   clientCounter += 1;
+function makePow(number, pow) {
+  // if (pow > 1)
+  // return number * makePow(number, pow - 1);
+  // return number;
+}
+
+const res = makePow(5, 3);
+console.log(res);
+
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+// alert(pow(2, 3)); // 8
+
+// const array = ['banan', 'banan', 'apple'];
+
+// /**
+//  * 1. написати функцію
+//  * 2. функція має приймати масив рядків
+//  * 3 функція повертає обєкт з кількістю кожного слова
+//  */
+
+// const calculateWords = (array) => {
+//   const result = {};
+
+//   return result
 // }
 
-// do {
-//   console.log(clientCounter);
-//   clientCounter += 1;
-// } while(clientCounter < maxClients)
+// Задача з codewars , чи є якесь рішення цієі задачі без використання Match?
+
+// Complete the solution so that it splits the string into
+// pairs of two characters.If the string contains an odd number
+// of characters then it should replace the missing second character
+// of the final pair with an underscore('_').
+
+// Examples:
+
+// * 'abc' => ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+/////////моє рішення
+
+// new RegExp('.{1,2}') === /.{1,2}/;
+("adsad'asdasd");
+
+function solution(str) {
+  var string = str;
+  const strToArr = string.match(/.{1,2}/g);
+  if (str === '') {
+    return [];
+  }
+
+  for (let index = 0; index < strToArr.length; index++) {
+    let elOfArr = strToArr[index];
+    if (elOfArr.length < 2) {
+      strToArr.pop(elOfArr);
+      elOfArr = elOfArr + '_';
+      strToArr.push(elOfArr);
+    }
+  }
+
+  return strToArr;
+}
+
+solution('abc');
+
+////рішення з codewars
+
+function solution(str) {
+  str.length % 2 && (str += '_');
+  return str.match(/.{1,2}/g) || [];
+}
+
+/////////
+
+// function solution(s){
+// return (s+"_").match(/.{2}/g)||[]
+// }
+console.log(solution('abcdefj')); // ['ab', 'cd', 'ef', 'j_']
+console.log(solution('')); // []
